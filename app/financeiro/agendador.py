0e3add7bb6e9983b487f5861929cfddf8959bfd0
HyppersLoyvenus <hyppersloyvenus@gmail.com>
2026-05-26 19:49:58 -0300

    Cron para verificação de email

--- app/financeiro/agendador.py
+++ app/financeiro/agendador.py
@@ -1,14 +1,16 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from app.financeiro.notificacoes import verificar_vencimentos
 
+
 def iniciar_agendador(app):
-        scheduler = BackgroundScheduler()
+    scheduler = BackgroundScheduler()
 
-        scheduler.add_job(
-            func=verificar_vencimentos,
-            trigger="interval",
-            minutes=1,
-            args=[app]
-        )
+    scheduler.add_job(
+        func=verificar_vencimentos,
+        trigger="cron",
+        hour=8,
+        minute=0,
+        args=[app],
+    )
 
-        scheduler.start()
+    scheduler.start()
